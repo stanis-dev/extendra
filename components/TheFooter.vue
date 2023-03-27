08978feee5e569f2a9d9e6acef3f4e68c040a64c
Stan <stanis.samisco@gmail.com>
2023-03-28 00:11:26 +0200

    disable links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,37 +3,37 @@ const menuItemsArr: Array<any> = [
   [
     'Directorio',
     [
-      { url: '/', name: 'Proyectos' },
-      { url: '/', name: 'Sectores' },
-      { url: '/', name: 'Usos' },
-      { url: '/', name: 'Nosotros' }
+      { url: '/', name: 'Proyectos', disabled: true },
+      { url: '/', name: 'Sectores', disabled: true },
+      { url: '/', name: 'Usos', disabled: true },
+      { url: '/', name: 'Nosotros', disabled: true }
     ]
   ],
   [
     'General',
     [
-      { url: '/', name: 'Blog' },
-      { url: '/', name: 'FAQ' },
-      { url: '/', name: 'Newsletter' },
-      { url: '/', name: 'Contacto' }
+      { url: '/', name: 'Blog', disabled: true },
+      { url: '/', name: 'FAQ', disabled: true },
+      { url: '/', name: 'Newsletter', disabled: true },
+      { url: '/contact', name: 'Contacto', disabled: false }
     ]
   ],
   [
     'Social',
     [
-      { url: '/', name: 'LinkedIn' },
-      { url: '/', name: 'Instagram' },
-      { url: '/', name: 'Twitter' },
-      { url: '/', name: 'Facebook' },
-      { url: '/', name: 'Vimeo' }
+      { url: '/', name: 'LinkedIn', disabled: true },
+      { url: '/', name: 'Instagram', disabled: true },
+      { url: '/', name: 'Twitter', disabled: true },
+      { url: '/', name: 'Facebook', disabled: true },
+      { url: '/', name: 'Vimeo', disabled: true }
     ]
   ],
   [
     'Legal',
     [
-      { url: '/', name: 'Privacidad' },
-      { url: '/', name: 'Cookies' },
-      { url: '/', name: 'Aviso Legal' }
+      { url: '/', name: 'Privacidad', disabled: true },
+      { url: '/', name: 'Cookies', disabled: true },
+      { url: '/legal', name: 'Aviso Legal', disabled: false }
     ]
   ]
 ]
@@ -62,8 +62,13 @@ const switchLocalePath = useSwitchLocalePath()
           </p>
           <ul>
             <li
+              data-cursor="hover"
               v-for="menuItem in menu[1]"
-              class="mb-6 last:mb-0 tablet:mb-2 text-lg leading-6"
+              :class="`mb-6 last:mb-0 tablet:mb-2 text-lg leading-6 ${
+                menuItem.disabled
+                  ? 'text-app-gray opacity-40 pointer-events-none'
+                  : 'text-black'
+              }`"
             >
               <NuxtLink :to="menuItem.url">{{ menuItem.name }}</NuxtLink>
             </li>
