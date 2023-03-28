e678de3ea143b006463034b82a6acc15129dc293
Stan <stanis.samisco@gmail.com>
2023-03-28 12:02:40 +0200

    as

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,12 @@ definePageMeta({
 
 <template>
   <div
-    class="h-screen w-screen p-10 pb-24 bg-white flex flex-col justify-center relative"
+    class="h-screen w-screen p-10 pb-24 bg-cream flex flex-col justify-center relative"
   >
     <BlackLogo
       class="w-[25vw] min-w-[400px] absolute top-1/2 left-1/2 -translate-x-1/2"
     />
-    <div class="text-cream text-body flex w-full justify-between mt-auto">
+    <div class="text-body flex w-full justify-between mt-auto">
       <p class="max-w-[220px] laptop:max-w-[240px] desktop:max-w-[250px]">
         Virtual realities, wide possibilities.
       </p>
