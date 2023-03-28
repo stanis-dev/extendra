b3d8af3ab63568a39a6ee235c772031d39c52b4c
Stan <stanis.samisco@gmail.com>
2023-03-28 11:59:31 +0200

    asd

--- components/Banner.vue
+++ components/Banner.vue
@@ -30,8 +30,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="wrapper relative" data-cursor-color="white">
+  <div class="wrapper relative">
     <div
+      data-cursor-color="white"
       ref="banner"
       class="bg-black absolute left-0 opacity-0 transition max-w-screen w-screen h-full p-4 laptop:relative pb-[85px] laptop:p-0 flex flex-col items-center laptop:justify-center"
     >
